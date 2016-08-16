7a633329c5f8acc6004f6449156ee0b47511adbf
bronwyndet <wayward@waywardcoffee.com>
2016-08-16 15:06:54 -0700

    new files added for day02 assignment, constructor function done, variable of data objects done

---
+++ js/build.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var entries = [];
+
+function Jobs (options) {
+  this.company = options.company;
+  this.role = options.role;
+  this.dates = options.dates;
+  this.description = options.description;
+};
