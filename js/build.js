3ff204e64a63272c830b346f53e0599a7b14d0c9
bronwyndet <wayward@waywardcoffee.com>
2016-08-16 16:58:04 -0700

    jquery objects rendering to page, but css not being recognized

--- js/build.js
+++ js/build.js
@@ -9,12 +9,23 @@ function Job (options) {
   this.writeup = options.writeup;
 };
 
-Jobs.prototype.toHtml = function() {
-  var $newJob = $('article.templateExp').clone();
-  $newJob.find('h3', this.company);
-  $newJob.find('h4', this.role);
-  $newJob.find('h4 span', this.dates);
-  $newJob.find('p', this.writeup);
+Job.prototype.toHtml = function() {
+  var $newJob = $('.template').clone();
+  $newJob.find('h3').text(this.company);
+  $newJob.find('h4').text(this.role);
+  // $newJob.find('h4 span').text(this.dates);
+  $newJob.find('p').text(this.writeup);
 
+  $newJob.removeClass('template');
 
+  return $newJob;
 };
+
+expEntries.forEach(function(options) {
+  entries.push(new Job(options));
+});
+
+entries.forEach(function(currentEntry) {
+  $('#experience').append(currentEntry.toHtml());
+  console.log(currentEntry);
+});
