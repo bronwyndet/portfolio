657e44e19996ada358c60ff1d85c0d6e68c0012c
bronwyndet <wayward@waywardcoffee.com>
2016-08-16 15:56:04 -0700

    progress on creating jquery object

--- js/build.js
+++ js/build.js
@@ -2,9 +2,19 @@
 
 var entries = [];
 
-function Jobs (options) {
+function Job (options) {
   this.company = options.company;
   this.role = options.role;
   this.dates = options.dates;
-  this.description = options.description;
+  this.writeup = options.writeup;
+};
+
+Jobs.prototype.toHtml = function() {
+  var $newJob = $('article.templateExp').clone();
+  $newJob.find('h3', this.company);
+  $newJob.find('h4', this.role);
+  $newJob.find('h4 span', this.dates);
+  $newJob.find('p', this.writeup);
+
+
 };
