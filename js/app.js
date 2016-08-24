bea6109e57ec41369a1caee0578b4e291e3b82e7
bronwyndet <wayward@waywardcoffee.com>
2016-08-24 06:05:15 -0700

    adding IIFE

--- js/app.js
+++ js/app.js
@@ -1,37 +1,43 @@
 'use strict';
 
-var navView = {};
+(function(module){
 
-// CLEARING PAGE OF CONTENT EXCEPT SKILLS (EQUATE TO HOME)
-$('.category').not('#skills').hide();
+  var navView = {};
 
-// SINGLE-PAGE NAVIGATION FUNCTION USING JQUERY
-navView.navigate = function() {
-  $('.nav').on('click', function() {
-    $('.category').hide();
-  });
+  // CLEARING PAGE OF CONTENT EXCEPT SKILLS (EQUATE TO HOME)
+  $('.category').not('#skills').hide();
 
-  $('.nav[data-category="skills"]').on('click', function() {
-    $('#skills').show();
-  });
+  // SINGLE-PAGE NAVIGATION FUNCTION USING JQUERY
+  navView.navigate = function() {
+    $('.nav').on('click', function() {
+      $('.category').hide();
+    });
 
-  $('.nav[data-category="projects"]').on('click', function() {
-    $('#projects').show();
-  });
+    $('.nav[data-category="skills"]').on('click', function() {
+      $('#skills').show();
+    });
 
-  $('.nav[data-category="experience"]').on('click', function() {
-    $('#experience').show();
-  });
+    $('.nav[data-category="projects"]').on('click', function() {
+      $('#projects').show();
+    });
 
-  $('.nav[data-category="interests"]').on('click', function() {
-    $('#interests').show();
-  });
+    $('.nav[data-category="experience"]').on('click', function() {
+      $('#experience').show();
+    });
 
-  $('.nav[data-category="profile"]').on('click', function() {
-    $('#profile').show();
-  });
-};
+    $('.nav[data-category="interests"]').on('click', function() {
+      $('#interests').show();
+    });
+
+    $('.nav[data-category="profile"]').on('click', function() {
+      $('#profile').show();
+    });
+  };
+
+  module.ProjectEntry = ProjectEntry;
 
 // CALLING SINGLE-PAGE NAVIGATION FUNCTION
-navView.navigate();
-ProjectEntry.retrieveAll();
+  navView.navigate();
+  ProjectEntry.retrieveAll();
+
+})(window);
