5e07ff226046da966bb0c8df0e9fcf5e3273cb6f
bronwyndet <wayward@waywardcoffee.com>
2016-08-30 06:56:34 -0700

    continuing to test with minor changes

--- js/app.js
+++ js/app.js
@@ -2,42 +2,42 @@
 
 (function(module){
 
-  var navView = {};
-
-  // CLEARING PAGE OF CONTENT EXCEPT SKILLS (EQUATE TO HOME)
-  $('.category').not('#skills').hide();
-
-  // SINGLE-PAGE NAVIGATION FUNCTION USING JQUERY
-  navView.navigate = function() {
-    $('.nav').on('click', function() {
-      $('.category').hide();
-    });
-
-    $('.nav[data-category="skills"]').on('click', function() {
-      $('#skills').show();
-    });
-
-    $('.nav[data-category="projects"]').on('click', function() {
-      $('#projects').show();
-    });
-
-    $('.nav[data-category="experience"]').on('click', function() {
-      $('#experience').show();
-    });
-
-    $('.nav[data-category="interests"]').on('click', function() {
-      $('#interests').show();
-    });
-
-    $('.nav[data-category="profile"]').on('click', function() {
-      $('#profile').show();
-    });
-  };
+  // var navView = {};
+  //
+  // // CLEARING PAGE OF CONTENT EXCEPT SKILLS (EQUATE TO HOME)
+  // $('.category').not('#skills').hide();
+  //
+  // // SINGLE-PAGE NAVIGATION FUNCTION USING JQUERY
+  // navView.navigate = function() {
+  //   $('.nav').on('click', function() {
+  //     $('.category').hide();
+  //   });
+  //
+  //   $('.nav[data-category="skills"]').on('click', function() {
+  //     $('#skills').show();
+  //   });
+  //
+  //   $('.nav[data-category="projects"]').on('click', function() {
+  //     $('#projects').show();
+  //   });
+  //
+  //   $('.nav[data-category="experience"]').on('click', function() {
+  //     $('#experience').show();
+  //   });
+  //
+  //   $('.nav[data-category="interests"]').on('click', function() {
+  //     $('#interests').show();
+  //   });
+  //
+  //   $('.nav[data-category="profile"]').on('click', function() {
+  //     $('#profile').show();
+  //   });
+  // };
 
   module.ProjectEntry = ProjectEntry;
 
 // CALLING SINGLE-PAGE NAVIGATION FUNCTION
-  navView.navigate();
+  // navView.navigate();
   ProjectEntry.retrieveAll();
 
 })(window);
